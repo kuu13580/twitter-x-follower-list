6f3854783f7437a26867c8d6db147e83e1001eb1
kuu13580 <13580kuu@gmail.com>
2023-12-17 02:59:08 +0900

    add 最新版との比較を実装

--- main.py
+++ main.py
@@ -2,30 +2,22 @@ from get_list import get_list
 from diff_report import diff_report
 from tkinter import filedialog
 import argparse
+import os
+import sys
 
-def main():
-    parser = argparse.ArgumentParser(description="Twitterのフォロー・フォロワー一覧を取得するスクリプト")
-    parser.add_argument("--mode", "-m", type=int, help="実行内容を選択 1:フォロワー取得 2:フォロー取得 3:差分表示 0:終了")
-    parser.add_argument("--files", "-f", type=str, help="比較するファイルのパスを指定", nargs=2, metavar=("previous_list_file", "new_list_file"))
-    # 引数を解析
-    args = parser.parse_args()
-
-    if args.mode == 3 and args.files is None:
-        print("ファイルを指定してください。")
-        return
-
-    if args.mode is not None:
-        print("実行内容を選択")
-        print("0. 終了")
-        print("1. フォロワー一覧を取得")
-        print("2. フォロー一覧を取得")
-        print("3. 差分を表示")
-        mode = int(input(">> "))
-    else:
-        mode = args.mode
-    if not mode in range(4):
+def mainGUI():
+    
+    print("実行内容を選択")
+    print("0. 終了")
+    print("1. フォロワー一覧を取得")
+    print("2. フォロー一覧を取得")
+    print("3. 差分を表示")
+    print("4. 最新版を直近の結果と比較")
+    mode = int(input(">> "))
+    if not mode in range(5):
         print("不正な入力です。")
         return
+    
     if mode == 0:
         print("終了します。")
         exit(0)
@@ -34,20 +26,78 @@ def main():
     elif mode == 2:
         get_list("follow")
     elif mode == 3:
-        if args.files is not None:
-            previous_list_file = args.files[0]
-            new_list_file = args.files[1]
-        else:
-            typ = [('テキストファイル', '*.txt')]
-            dir = './'
-            print("比較元データを選択してください。")
-            previous_list_file = filedialog.askopenfilename(title="比較元データを選択", filetypes = typ, initialdir = dir)
-            print("比較先データを選択してください。")
-            new_list_file = filedialog.askopenfilename(title="比較先データを選択", filetypes = typ, initialdir = dir)
+        typ = [('テキストファイル', '*.txt')]
+        dir = './'
+        print("比較元データを選択してください。")
+        previous_list_file = filedialog.askopenfilename(title="比較元データを選択", filetypes = typ, initialdir = dir)
+        print("比較先データを選択してください。")
+        new_list_file = filedialog.askopenfilename(title="比較先データを選択", filetypes = typ, initialdir = dir)
         diff_report(previous_list_file, new_list_file)
+    elif mode == 4:
+        print("比較対象を選択してください。(follow or follower)")
+        recent = input(">> ")
+        if recent not in ["follow", "follower"]:
+            print("不正な入力です。")
+            return
+        files = os.listdir()
+        files = [file for file in files if file.startswith(recent + "_") and file.endswith(".txt")]
+        if len(files) == 0:
+            print("ファイルが見つかりませんでした。")
+            return
+        files.sort()
+        recent_file = files[-1]
+        new_file = get_list(recent)
+        diff_report(recent_file, new_file)
+
+def mainCLI(args):
+
+    # 引数チェック
+    if args.mode is None:
+        print("実行内容を選択してください。")
+        return
+    mode = args.mode
+    
+    if mode == 0:
+        print("終了します。")
+        exit(0)
+    elif mode == 1:
+        get_list("follower")
+    elif mode == 2:
+        get_list("follow")
+    elif mode == 3:
+        if args.files is None:
+            print("ファイルを指定してください。")
+            return
+        previous_list_file = args.files[0]
+        new_list_file = args.files[1]
+        diff_report(previous_list_file, new_list_file)
+    elif mode == 4:
+        if args.recent is None:
+            print("比較対象を指定してください。")
+            return
+        recent = args.recent
+        files = os.listdir()
+        files = [file for file in files if file.startswith(recent + "_") and file.endswith(".txt")]
+        if len(files) == 0:
+            print("ファイルが見つかりませんでした。")
+            return
+        files.sort()
+        recent_file = files[-1]
+        new_file = get_list(recent)
+        diff_report(recent_file, new_file)
 
 if __name__ == "__main__":
-
-    while True:
-        main()
-        print()
+    parser = argparse.ArgumentParser(description="Twitterのフォロー・フォロワー一覧を取得するスクリプト")
+    parser.add_argument("--mode", "-m", type=int, help="実行内容を選択 1:フォロワー取得 2:フォロー取得 3:差分表示 4:最新版比較 0:終了", choices=range(5))
+    parser.add_argument("--files", "-f", type=str, help="比較するファイルのパスを指定", nargs=2, metavar=("previous_list_file", "new_list_file"))
+    parser.add_argument("--recent", "-r", type=str, help="最新版比較の対象指定", metavar=("follow or follower"), choices=["follow", "follower"])
+    # 引数を解析
+    args = parser.parse_args()
+    print(len(sys.argv))
+    # 引数がない場合はGUIで実行
+    if len(sys.argv) == 1:
+        while True:
+            mainGUI()
+            print()
+    else:
+        mainCLI(args)
